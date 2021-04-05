2351db8abae7ce325847e2a3d9bc8bc49203ed9f
StealthHydra179 <47306082+StealthHydra179@users.noreply.github.com>
2021-04-05 13:56:10 -0400

    Update base.cpp

--- src/base.cpp
+++ src/base.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     cout << "Base Start\n";
     //Content
-
+    block("block1");
 
     cout << "Base End\n";
 }
@@ -22,11 +22,15 @@ class block {
 
 public:
 
-    String name; //temp
-    block() {
-    }
-    block(String name) {
-        this.name = name;
+    const String name; //temp
+
+    explicit block(const String& name = null) {
+        if (name != null) {
+            this->name = name;
+        } else {
+            this->name = "default";
+        }
+
     }
 
 };
