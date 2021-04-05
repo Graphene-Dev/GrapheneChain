b0c4ae7ba49e6b1f76a2490df27b250055e41433
TechyGuy <65623992+TechyGuy17@users.noreply.github.com>
2021-04-05 17:54:35 +0200

    Do not question this

---
+++ src/crypto.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+cout << "Does some crypto stuff";
+cout << "Mines some crypto";
+cout << "congrats you got 20 graphene";
+cout << "Bye";
+}
