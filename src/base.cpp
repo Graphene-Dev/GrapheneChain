898bb53a5b06609e48a7a3f345d5aa207713fdc8
VA7NFH <va7nfh@gmail.com>
2021-04-06 14:29:02 -0700

    stealth bad at linux

--- src/base.cpp
+++ src/base.cpp
@@ -243,7 +243,11 @@ int main(int argc, char *argv[]) {
 //        cout << e.what();
 //    }
     while (running) {
-            Sleep(10000);
+        #ifdef _WIN32
+            _sleep(10000);
+        #else
+            usleep(10000);
+        #endif
 
 
         chain.addBlock(block);
