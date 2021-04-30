2008edc193e0a778a4926092bd8abd320417180a
Teo Lundgren <teo.lund1523@gmail.com>
2021-04-30 15:53:32 +0200

    some space inbetween looks good

--- src/base.cpp
+++ src/base.cpp
@@ -40,6 +40,7 @@ bool running = true;
 void userActions() {
     while (true) {
         cout << "The availible commands are: \n newTransacion \n displayChain \n displayCurrentBlock \n hashblock OBS! only for testing \n";
+        cout << "\n";
         cout << "Next action? ";
         string input;
         cin >> input;
