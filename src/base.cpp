4deab948de6a36db63ddbe04010a8c82412f2932
Teo Lundgren <teo.lund1523@gmail.com>
2021-04-30 16:26:28 +0200

    Added a bat script to compile the code on windows for the lazy

--- src/base.cpp
+++ src/base.cpp
@@ -39,8 +39,8 @@ bool running = true;
 
 void userActions() {
     while (true) {
-        cout << "The availible commands are: \n newTransacion \n displayChain \n displayCurrentBlock \n hashblock (OBS! only for testing) \n";
-        cout << "\n";
+        cout << "The availible commands are: \n newTransacion \n displayChain \n displayCurrentBlock \n hashblock OBS! only for testing \n";
+        cout << "";
         cout << "Next action? ";
         string input;
         cin >> input;
---
+++ winCompile.bat
@@ -0,0 +1,8 @@
+@echo off
+echo "This commands run the commands in README for you, if it failes make sure you got cmake and visual studio 2019 and its c++ plugins downloaded"
+sleep 5
+mkdir build
+cd build 
+cmake -G "Visual Studio 16 2019" -A x64 ..
+MSBuild GrapheneChain.sln
+echo "Compiling is done! Now the files should be in Debug or Release"
